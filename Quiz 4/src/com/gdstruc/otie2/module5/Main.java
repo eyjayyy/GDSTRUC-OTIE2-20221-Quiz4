48cff8d8ebc7e6b8eeb7a8d7922c3b53b475c8c9
Aleyne Jasmin Galicia <aleynejasmin.galicia@benilde.edu.ph>
2022-11-14 10:53:17 +0800

    Added Quiz 4 Answer

---
+++ Quiz 4/src/com/gdstruc/otie2/module5/Main.java
@@ -0,0 +1,31 @@
+package com.gdstruc.otie2.module5;
+
+public class Main {
+    public static void main(String[] args) {
+        Player pikachu = new Player(25, "Pikachu", 1);
+        Player celebi = new Player(251, "Celebi", 2);
+        Player hitmonchan = new Player(107, "Hitmonchan", 3);
+        Player mankey = new Player(56, "Mankey", 4);
+        Player starmie = new Player(175, "Starmie", 4);
+
+        SimpleHashtable hashtable = new SimpleHashtable();
+
+        // add initial players
+        hashtable.put(celebi.getUserName(), celebi);
+        hashtable.put(mankey.getUserName(), mankey);
+        hashtable.put(pikachu.getUserName(), pikachu);
+        hashtable.put(hitmonchan.getUserName(), hitmonchan);
+        hashtable.put(starmie.getUserName(), starmie);
+
+        // print the contents of the hashtable
+        hashtable.printHashtable();
+
+        // remove player using key and print the updated hashtable
+        System.out.println("\nAfter removing player:");
+        hashtable.remove("Pikachu");
+        hashtable.printHashtable();
+
+        // get a value based on a given key
+        //System.out.println(hashtable.get("Mankey"));
+    }
+}
